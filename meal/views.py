02a51744e6d75ee403d723f459e902864f996231
nada <nadasamir9334@gmail.com>
2023-08-10 22:01:29 -0400

    remove auth import in views.py

--- meal/views.py
+++ meal/views.py
@@ -5,8 +5,8 @@ from .serializers import Mealserializers,Rateserializers,Userserializers
 from rest_framework.decorators import action
 from django.contrib.auth.models import User
 from rest_framework.response import Response
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import AllowAny,IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
+# from rest_framework.authentication import TokenAuthentication
+# from rest_framework.permissions import AllowAny,IsAuthenticated,IsAdminUser,IsAuthenticatedOrReadOnly
 
 
 
